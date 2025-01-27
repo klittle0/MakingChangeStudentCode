bd9d43c032a4401bbd4d67ca9fc5c798963f64b9
kate <kate.little@menloschool.org>
2025-01-27 13:44:08 -0800

    1/27 — working exponential draft

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -15,51 +15,34 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
-        // Arraylist of lists — each list will be of length coins
-        // Each index will represent the # of each type of coin
-        ArrayList<int[]> solutions = new ArrayList<>();
-        int[] maxCounts = new int[coins.length];
-        // Sort in ascending order
+        // Sort coins arrays
         Arrays.sort(coins);
-
-        //Fill in max counts, which represents the max # of each coin that can fit in target
-        for (int i = coins.length - 1; i >= 0 ; i--){
-            int numCoin = 0;
-            // Finds max # of this coin that can fit into target
-            while (numCoin * coins[i] < target){
-                numCoin++;
-            }
-            maxCounts[i] = numCoin;
-        }
-
-        //Rely on MaxCounts to find all solutions:
+        long count = 0;
         for (int i = 0; i < coins.length; i++){
-            for (int j = 0; j < coins[i]; j++){
-                // Try to find all solutions w/ all values of these coins
-                int[] current = new int[coins.length];
-                current[i] = j;
-                coinRecurse(target, j * coins[i], coins, current, solutions);
-            }
+            count += coinRecurse(target, coins[i], i, coins);
         }
-        return 0;
+        return count;
     }
-
-
-    //Pseudocode:
-    //start w/ a coin type & see how many of that type you can fit
-    // Then, go to the next coin type & try to fill in the gaps
-    // If not possible, go to the next
 
     // Or maybe I should do a recursive method...I could basically just go through each type of coin, see how
 
-    public static void coinRecurse(int target, int total, int[] coins, int[] current, ArrayList<int[]> solutions){
+    public static int coinRecurse(int target, int total, int currentIndex, int[] coins, int[] coinCount){
         // Base case — if target is reached
         if (total == target){
-            solutions.add(current);
-        }
-        // Base case — if it's not possible to reach the target w/ current values, return
 
-        // for
+            return 1;
+        }
+        // Base case — if value is greater than target, it's not a valid solution
+        if (total > target){
+            // Correct return statement?
+            return 0;
+        }
+        // Recurse
+        int count = 0;
+        for (int i = currentIndex; i < coins.length; i++){
+            count += coinRecurse(target, total + coins[i], i, coins);
+        }
+        return count;
     }
 
 }
